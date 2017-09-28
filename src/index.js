c5e0d97c3e2582c526eda17b5ab79efa7ea752ba
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 09:51:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+class meInReact extends React.Component{
+  render(){
+    return(
+      <div class="me">
+        <h1>An Awesome Person</h1>
+        <p>Who is learning React</p>
+
+        <ul class="my-interests">
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Movies</li>
+          <li>Ice cream</li>
+        </ul>
+      </div>
+    )
+  }
+}
 ReactDOM.render(
-  meInReact, 
+  <meInReact />,
   document.getElementById('root')
 );
 
 export default meInReact
-*/
